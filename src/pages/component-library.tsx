00b9937d1cd642e60a70f05d0b1dc0b2ee627e3d
RomanVelky <rom.velky@gmail.com>
2024-05-02 17:09:28 +0200

    card input label select

--- src/pages/component-library.tsx
+++ src/pages/component-library.tsx
@@ -6,11 +6,30 @@ import {
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Mail } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function ComponentLibrary() {
   return (
     <>
       <div>ComponentLibrary</div>
+
+      {/*ACCORDION  */}
       <div className="py-11 px-11">
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
@@ -23,25 +42,71 @@ export default function ComponentLibrary() {
           <AccordionItem value="item-2">
             <AccordionTrigger>Why is that?</AccordionTrigger>
             <AccordionContent>
-              I really don't know. I just wanted to show off the accordion
+              I really dont know. I just wanted to show off the accordion
             </AccordionContent>
           </AccordionItem>
         </Accordion>
       </div>
-      <div>
-        <Button>Primary</Button>
+
+      {/* BUTTONS */}
+      <div className="flex justify-between px-11 py-3">
+        <div>
+          <Button>Primary</Button>
+        </div>
+        <div>
+          <Button variant="secondary">Secondary</Button>
+        </div>
+        <div>
+          <Button variant="destructive">destructive</Button>
+        </div>
+        <div>
+          <Button>
+            <Mail className="mr-2 h-4 w-4" /> Login with Email
+          </Button>
+        </div>
       </div>
-      <div>
-        <Button variant="secondary">Secondary</Button>
+
+      {/* CARD */}
+      <div className="px-11 py-4">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Name of your project" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                  <Select>
+                    <SelectTrigger id="framework">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="next">Next.js</SelectItem>
+                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                      <SelectItem value="astro">Astro</SelectItem>
+                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
       </div>
-      <div>
-        <Button variant="destructive">destructive</Button>
-      </div>
-      <div>
-        <Button>
-          <Mail className="mr-2 h-4 w-4" /> Login with Email
-        </Button>
-      </div>
+
+      {/* ALERT  TODO */}
     </>
   );
 }
