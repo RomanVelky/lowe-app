c71bc30dd3dfb8462e863bc795eeadeb703490a6
RomanVelky <roman.velky@proton.me>
2024-05-10 19:17:17 +0200

    fix: git name change

--- src/pages/wages.tsx
+++ src/pages/wages.tsx
@@ -2,7 +2,7 @@ export default function Wages() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <div className="py-11 px-11">
-        <h1>Wages page</h1>
+        <h1>Wages Page</h1>
       </div>
     </main>
   );
