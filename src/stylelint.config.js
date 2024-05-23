6e239341a2e826dcea3c2f40f742ae16c5c5037b
RomanVelky <roman.velky@proton.me>
2024-05-24 01:07:46 +0200

    feat: stylelint added

---
+++ src/stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-vue",
+    "stylelint-config-tailwindcss",
+  ],
+  rules: {
+    // Add any custom rules here
+  },
+};
