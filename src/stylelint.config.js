f95ac66c169c6f630d984e68b174911b9bc46b35
RomanVelky <roman.velky@proton.me>
2024-07-14 23:24:45 +0200

    fix: stylelint error

--- src/stylelint.config.js
+++ src/stylelint.config.js
@@ -1,6 +1,7 @@
-module.export = {
-  extends: ["stylelint-config-standard", "stylelint-config-tailwindcss"],
-  rules: {
-    // Add any custom rules here
-  },
+export const extendsArray = [
+  "stylelint-config-standard",
+  "stylelint-config-tailwindcss",
+];
+export const rules = {
+  // Add any custom rules here
 };
