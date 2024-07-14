e365fa907f0762a4c9a5d7f0a471274b3b26a962
RomanVelky <roman.velky@proton.me>
2024-07-14 20:40:01 +0200

    feat: Toaster added to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "./footer";
 import Header from "./header";
 import { ReactNode } from "react";
+import { Toaster } from "../ui/toaster";
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,7 +11,12 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <main className="max-w-screen-xl mx-auto py-14">{children}</main>
+      <div>
+        <main className="max-w-md sm:max-w-2xl md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-xl mx-auto py-14">
+          {children}
+        </main>
+        <Toaster />
+      </div>
       <Footer />
     </>
   );
